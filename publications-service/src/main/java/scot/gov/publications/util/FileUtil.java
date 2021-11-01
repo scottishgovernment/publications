40c7f36bd4a30d309d5dbed0a1205c143ee088d1
David Sinclair <david.sinclair@gov.scot>
2021-11-01 14:00:21 +0000

    MGS-6049 write test fixtures to target directory
    write test fixtures used in unit tests to target/tmp rather
    than the system temp directory to avoid filling up jenkins
    disk

--- publications-service/src/main/java/scot/gov/publications/util/FileUtil.java
+++ publications-service/src/main/java/scot/gov/publications/util/FileUtil.java
@@ -9,15 +9,26 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.SecureRandom;
 
 public class FileUtil {
 
-    public File createTempFile(String prefix, FileType type) throws IOException {
-        return createTempFile(prefix, type.getExtension());
+    private static final SecureRandom random = new SecureRandom();
+
+    public String hash(File zip) throws IOException {
+        InputStream in = null;
+        try {
+            in = new FileInputStream(zip);
+            return BinaryUtils.toHex(Md5Utils.computeMD5Hash(in));
+        } finally {
+            IOUtils.closeQuietly(in);
+        }
     }
 
-    public File createTempFile(String prefix, String extension) throws IOException {
-        return File.createTempFile(prefix + "-tmp-", "." + extension);
+    public File createTempFile(String prefix, FileType type) throws IOException {
+        return createTempFile(prefix, type.getExtension());
     }
 
     public File createTempFile(String prefix, FileType type, InputStream content) throws IOException {
@@ -37,13 +48,21 @@ public class FileUtil {
         }
     }
 
-    public String hash(File zip) throws IOException {
-        InputStream in = null;
-        try {
-            in = new FileInputStream(zip);
-            return BinaryUtils.toHex(Md5Utils.computeMD5Hash(in));
-        } finally {
-            IOUtils.closeQuietly(in);
-        }
+    public File createTempFile(String prefix, String extension) throws IOException {
+        return new File(
+                tempDirectory(),
+                randomFilename(prefix, extension));
     }
+
+    private String randomFilename(String prefix, String extension) {
+        long randomNumber = Math.abs(random.nextLong());
+        return  prefix + Long.toString(randomNumber) + "." + extension;
+    }
+
+    private File tempDirectory() {
+        File dir = Paths.get("target", "tmp", "testfixtures").toFile();
+        dir.mkdirs();
+        return dir;
+    }
+
 }
