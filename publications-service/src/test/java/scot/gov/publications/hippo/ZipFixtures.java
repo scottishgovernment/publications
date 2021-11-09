3abaf4572cd2d819d1451b05fc8d803a08ded681
Martin Ellis <martin.ellis@gov.scot>
2021-11-09 12:59:30 +0000

    wip

--- publications-service/src/test/java/scot/gov/publications/hippo/ZipFixtures.java
+++ publications-service/src/test/java/scot/gov/publications/hippo/ZipFixtures.java
@@ -1,23 +1,21 @@
 package scot.gov.publications.hippo;
 
-import org.apache.commons.io.Charsets;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.*;
-import java.security.SecureRandom;
-import java.util.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-
-import static org.apache.commons.lang3.StringUtils.startsWith;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFixtures {
-
-    private static final SecureRandom random = new SecureRandom();
 
     private static final Logger LOG = LoggerFactory.getLogger(ZipFixtures.class);
 
@@ -45,97 +43,100 @@ public class ZipFixtures {
      * Create a zip from a directory
      */
     public static ZipFile zipDirectory(Path source) throws IOException {
+        Path path = Files.createTempFile(fixturesDirectory(), "zip", ".zip");
+        ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(path.toFile()));
+        Path namePrefix = Paths.get("directory");
+        Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                String name = namePrefix.resolve(source.relativize(dir)) + "/";
+                zos.putNextEntry(new ZipEntry(name));
+                return super.preVisitDirectory(dir, attrs);
+            }
 
-        // create a temporary file that will become the zip
-        Path zipPath = Files.createTempFile("zipDirectory", ".zip");
-
-        // now zip up the contents of the temp directory
-        FileSystem zipfs = zipFileSystem(zipPath);
-
-        // create a sub directory within the zip since the importer expected this structure
-        Path directory = zipfs.getPath("directory");
-        Files.createDirectory(directory);
-
-        // copy files in the sourceDir into the zip
-        for (String file : source.toFile().list()) {
-            Files.copy(source.resolve(file), directory.resolve(file), StandardCopyOption.REPLACE_EXISTING);
-        }
-        zipfs.close();
-
-        // create a zip file from that directory
-        return new ZipFile(zipPath.toFile());
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                String name = namePrefix.resolve(source.relativize(file)).toString();
+                zos.putNextEntry(new ZipEntry(name));
+                Files.copy(file, zos);
+                zos.closeEntry();
+                return super.visitFile(file, attrs);
+            }
+        });
+        zos.close();
+        return new ZipFile(path.toFile());
     }
 
-    public static void deleteFixtures() {
-        File tmpdir = new File(System.getProperty("java.io.tmpdir"));
-        File [] zips = tmpdir.listFiles(file -> startsWith(file.getName(), "zipDirectory"));
-        for (File zip : zips) {
-            LOG.info("git tae f {}", zip.getAbsolutePath());
-            zip.delete();
+    public static void deleteFixtures() throws IOException {
+        Path path = fixturesDirectory();
+        if (!Files.exists(path)) {
+            return;
+        }
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (!file.equals(path)) {
+                    Files.delete(file);
+                }
+                return super.visitFile(file, attrs);
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                return super.postVisitDirectory(dir, exc);
+            }
+        });
+    }
+
+    private static Path fixturesDirectory() {
+        URL url = ZipFixtures.class.getProtectionDomain().getCodeSource().getLocation();
+        URI uri = toUri(url);
+        return Paths.get(uri).getParent().resolve("fixtures");
+    }
+
+    public static URI toUri(URL url) {
+        try {
+            return url.toURI();
+        } catch (URISyntaxException ex) {
+            throw new RuntimeException(ex);
         }
     }
 
-    private static FileSystem zipFileSystem(Path path) throws IOException {
-        Map<String, String> env = new HashMap<>();
-        env.put("create", "true");
-        Files.delete(path);
-        URI uri = URI.create("jar:file:" + path);
-        return FileSystems.newFileSystem(uri, env);
+    public static Path copyFixture(String prefix) throws IOException {
+        return copyFixture(prefix, "/fixtures/exampleZipContents");
     }
 
-    /**
-     * Copy a fixture contained in a resources folder to a temp directory.  This can then be manpulated to create the
-     * scenario being tested and then zipped ready for a test.
-     */
-    public static Path copyFixtureToTmpDirectory(String prefix, String zipContentPath) throws IOException {
-        Path tempDirWithPrefix = createTempDirectory(prefix);
-
-        LOG.info("copyFixtureToTmpDirectory path is {}", tempDirWithPrefix);
-        InputStream resourceDirStream = ZipFixtures.class.getClassLoader().getResourceAsStream(zipContentPath);
-        List<String> filenames = IOUtils.readLines(resourceDirStream, Charsets.UTF_8);
-
-        for (String filename : filenames) {
-            String path = "/" + zipContentPath + "/" + filename;
-            File dest = tempDirWithPrefix.resolve(filename).toFile();
-            InputStream resourceInputStream = ZipFixtures.class.getResourceAsStream(path);
-            FileUtils.copyInputStreamToFile(resourceInputStream, dest);
+        /**
+         * Copy a fixture contained in a resources folder to a temp directory.  This can then be manpulated to create the
+         * scenario being tested and then zipped ready for a test.
+         */
+    public static Path copyFixture(String prefix, String zipContentPath) throws IOException {
+        Path path = fixturesDirectory();
+        if (!Files.exists(path)) {
+            Files.createDirectory(path);
         }
+        Path directory = Files.createTempDirectory(path, prefix);
+        LOG.info("copyFixtureToTmpDirectory path is {}", directory);
+        URI source = toUri(ZipFixtures.class.getResource(zipContentPath));
+        copyRecursively(Paths.get(source), directory);
+        return directory;
+    }
 
-        return tempDirWithPrefix;
+    private static void copyRecursively(Path source, Path target) throws IOException {
+        Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.copy(file, target.resolve(source.relativize(file)));
+                return super.visitFile(file, attrs);
+            }
+        });
     }
 
     static ZipFile zip(String name) throws IOException {
-        File exampleFile = createTempFile(name, "zip");
-        InputStream in = ImageUploaderTest.class.getResourceAsStream("/" + name + ".zip");
-        OutputStream out = new FileOutputStream(exampleFile);
-        IOUtils.copy(in, out);
-        return new ZipFile(exampleFile);
-    }
-
-    static File createTempFile(String prefix, String extension) throws IOException {
-        Path textFixturesDirPath = textFixturesPath();
-        textFixturesDirPath.toFile().mkdirs();
-        Path path = textFixturesDirPath.resolve(randomFilename(prefix, extension));
-        return Files.createFile(path).toFile();
-    }
-
-    static Path createTempDirectory(String prefix) {
-        String dirname = randomFilename(prefix);
-        Path dir = textFixturesPath().resolve(dirname);
-        dir.toFile().mkdirs();
-        return dir;
-    }
-
-    static String randomFilename(String prefix) {
-        return String.format("%s%d", prefix, Math.abs(random.nextLong()));
-    }
-
-    static String randomFilename(String prefix, String extention) {
-        return String.format("%s.%s", randomFilename(prefix), extention);
-    }
-
-    static Path textFixturesPath() {
-        return Paths.get("target", "tmp", "testfixtures");
+        URL resource = ZipFixtures.class.getResource("/" + name + ".zip");
+        File file = new File(toUri(resource));
+        return new ZipFile(file);
     }
 
 }
