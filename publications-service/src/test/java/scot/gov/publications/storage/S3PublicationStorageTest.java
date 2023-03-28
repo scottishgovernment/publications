34176a95a20d9c4972df3a8290b98158de5debd1
Martin Ellis <martin.ellis@gov.scot>
2023-03-28 23:32:11 +0100

    Upgrade AWS SDK version to a version that supports IMDSv2

--- publications-service/src/test/java/scot/gov/publications/storage/S3PublicationStorageTest.java
+++ publications-service/src/test/java/scot/gov/publications/storage/S3PublicationStorageTest.java
@@ -2,7 +2,6 @@ package scot.gov.publications.storage;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.DeleteObjectsResult;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3Object;
@@ -72,7 +71,7 @@ public class S3PublicationStorageTest {
 
         // ARRANGE
         S3PublicationStorage sut = new S3PublicationStorage();
-        sut.s3 = mock(AmazonS3Client.class);
+        sut.s3 = mock(AmazonS3.class);
         when(sut.s3.doesObjectExist(any(), any())).thenThrow(new AmazonClientException(""));
         sut.configuration = new S3();
         sut.configuration.setPath("path");
@@ -91,7 +90,7 @@ public class S3PublicationStorageTest {
         sut.configuration = new S3();
         sut.configuration.setPath("path");
         sut.configuration.setBucket("bucket");
-        sut.s3 = mock(AmazonS3Client.class);
+        sut.s3 = mock(AmazonS3.class);
         Publication publication = new Publication();
         publication.setChecksum("checksum");
         File file = mock(File.class);
@@ -115,7 +114,7 @@ public class S3PublicationStorageTest {
         sut.configuration = new S3();
         sut.configuration.setPath("path");
         sut.configuration.setBucket("bucket");
-        sut.s3 = mock(AmazonS3Client.class);
+        sut.s3 = mock(AmazonS3.class);
         when(sut.s3.putObject(any())).thenThrow(new AmazonClientException(""));
         Publication publication = new Publication();
         publication.setChecksum("checksum");
@@ -135,7 +134,7 @@ public class S3PublicationStorageTest {
         sut.configuration = new S3();
         sut.configuration.setPath("path");
         sut.configuration.setBucket("bucket");
-        sut.s3 = mock(AmazonS3Client.class);
+        sut.s3 = mock(AmazonS3.class);
         S3Object s3Obj = mock(S3Object.class);
         when(sut.s3.getObject(any())).thenReturn(s3Obj);
         Publication publication = new Publication();
@@ -160,7 +159,7 @@ public class S3PublicationStorageTest {
         sut.configuration = new S3();
         sut.configuration.setPath("path");
         sut.configuration.setBucket("bucket");
-        sut.s3 = mock(AmazonS3Client.class);
+        sut.s3 = mock(AmazonS3.class);
         S3Object s3Obj = mock(S3Object.class);
         when(sut.s3.getObject(any())).thenThrow(new AmazonClientException(""));
         Publication publication = new Publication();
@@ -185,7 +184,7 @@ public class S3PublicationStorageTest {
         sut.configuration = new S3();
         sut.configuration.setPath("path");
         sut.configuration.setBucket("bucket");
-        sut.s3 = mock(AmazonS3Client.class);
+        sut.s3 = mock(AmazonS3.class);
 
         ObjectListing objectListing1 = listing(true, summary("summary1"), summary("summary2"));
         when(sut.s3.listObjects(any(), any())).thenReturn(objectListing1);
@@ -210,7 +209,7 @@ public class S3PublicationStorageTest {
         sut.configuration = new S3();
         sut.configuration.setPath("path");
         sut.configuration.setBucket("bucket");
-        sut.s3 = mock(AmazonS3Client.class);
+        sut.s3 = mock(AmazonS3.class);
 
         when(sut.s3.listObjects(any(), any())).thenThrow(new AmazonClientException("arg"));
 
@@ -228,7 +227,7 @@ public class S3PublicationStorageTest {
         sut.configuration = new S3();
         sut.configuration.setPath("path");
         sut.configuration.setBucket("bucket");
-        sut.s3 = mock(AmazonS3Client.class);
+        sut.s3 = mock(AmazonS3.class);
         DeleteObjectsResult res = mock(DeleteObjectsResult.class);
         when(sut.s3.deleteObjects(any())).thenReturn(res);
         List<String> keys = new ArrayList<>();
